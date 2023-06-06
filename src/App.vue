38bb06e33611490ce83d721c3975d8304a7ea108
Leino <leino.dev@gmail.com>
2023-06-06 23:16:41 +0900

    いじってみた

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default{
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      title: "もふもふこそ正義の国",
+    }
+  }
+}
 </script>
 
 <template>
@@ -8,11 +18,12 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <div>{{ title }}</div>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/mofu">もふもふ</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/MofuView.vue
@@ -0,0 +1,30 @@
+<script>
+export default{
+  data() {
+    return {
+      animal: ["うさぎ", "ハムスター", "チンチラ"],
+      isShow: true
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="about">
+    <div v-if="isShow">
+      <ul>
+        <li v-for="item in animal" :key="item">{{item}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
