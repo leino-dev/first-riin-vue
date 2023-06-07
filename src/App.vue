0b5d864ecae0dd850cc0f6acd0d38ac667a3f880
Leino <leino.dev@gmail.com>
2023-06-07 22:11:57 +0900

    2日目の実習。

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,69 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// import HelloWorld from './components/HelloWorld.vue';
 
 export default{
   components: {
-    HelloWorld
+    // HelloWorld
   },
   data() {
     return {
       title: "もふもふこそ正義の国",
+      isShow: true,
+      count: 0,
+      hoge: "mofumofu",
+      isDisabled: true
+    }
+  },
+  methods: {
+    incremenet() {
+      // ここで変数を触る場合は、thisが必要
+      this.count++
+    }
+  },
+  computed: {
+    double() {
+      return this.count * 2
     }
   }
 }
 </script>
 
 <template>
+  <!-- ここ以降で変数を触る場合はthis不要 -->
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
+      <div class="my-wrap">
       <div>{{ title }}</div>
+
+      <div v-if="isShow">
+        <!-- hogeを値とするinputを作成 -->
+        <!-- エレメントのdisabledの設定に変数isDisabledを使う -->
+        <input v-model="hoge" :disabled="isDisabled"/>
+        <!-- 変数hogeの値を表示する -->
+        {{ hoge }}
+      </div>
+
+      <!-- clickイベントでisDisabledの値を変更する -->
+      <button @click="isDisabled = !isDisabled">編集とぐる</button>
+
+      <!-- :がないとただの文字列がclassに指定された扱い -->
+      <div class="hoge">もふん</div>
+
+      <!-- :があると変数hogeがclassに指定された扱い -->
+      <div :class="hoge">もふん</div>
+
+      <div>
+        <!-- countを表示する -->
+        うさぎが{{ count }}羽
+        <!-- methodのincremenetを実行する -->
+        <button @click="incremenet">+1</button>
+      </div>
+
+      <!-- computedのdouble()を実行した結果を表示する -->
+      <div>ハムスターが{{ double }}匹</div>
+    </div>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -32,6 +77,16 @@ export default{
 </template>
 
 <style scoped>
+.mofumofu {
+  background-color: rgb(178, 150, 73);
+}
+
+.my-wrap {
+  display:flex;
+  flex-flow: column;
+  max-width: 300px;
+}
+
 header {
   line-height: 1.5;
   max-height: 100vh;
