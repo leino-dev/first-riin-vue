556700410e2fc6657234ef18cec5d0e9f0f7e652
Leino <leino.dev@gmail.com>
2023-06-13 01:07:42 +0900

    縦のリスト表示

--- src/views/LineView.vue
+++ src/views/LineView.vue
@@ -1,23 +1,35 @@
 <script>
+import WelcomeItem from '../components/WelcomeItem.vue';
+
 export default{
+  components: {
+    WelcomeItem
+  },
   data() {
     return {
+      list: [
+        {icon: "@/assets/img/apps.svg"},
+        {icon: "@/assets/img/apps.svg"},
+        {icon: "@/assets/img/apps.svg"}
+      ]
     }
   }
 }
 </script>
 
 <template>
-  <div class="about">
+  <div>
+    <!-- divに入れて1つにしておかないとバラけた -->
+    <WelcomeItem v-for="item in list" :key="item">
+      <template #icon>
+        <img src="@/assets/img/apps.svg" />
+      </template>
+      <template #heading>Documentation</template>
+
+      Vue’s
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
+      provides you with all information you need to get started.
+    </WelcomeItem>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
