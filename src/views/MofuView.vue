3c45f669f42069a287a2213ee0546184d0d183fe
Leino <leino.dev@gmail.com>
2023-06-13 00:12:55 +0900

    オブジェクトを表示。

--- src/views/MofuView.vue
+++ src/views/MofuView.vue
@@ -3,6 +3,12 @@ export default{
   data() {
     return {
       animal: ["うさぎ", "ハムスター", "チンチラ"],
+      rabbit: {
+        head: "丸くてかわいい",
+        ears: "長くてかわいい",
+        hand: "ふわふわでかわいい",
+        belly: "ふかふか真っ白天使"
+      },
       isShow: true
     }
   }
@@ -13,7 +19,16 @@ export default{
   <div class="about">
     <div v-if="isShow">
       <ul>
-        <li v-for="item in animal" :key="item">{{item}}</li>
+        <!-- arrayをforぐるぐる -->
+        <li v-for="(item, index) in animal" :key="item">{{ index }}: {{ item }}</li>
+      </ul>
+      </div>
+
+    <div>
+      うさぎのかわいいところ
+      <ul>
+        <!-- objctをforぐるぐる -->
+        <li v-for="(item, key) in rabbit" :key="item">{{ key }}: {{ item }}</li>
       </ul>
     </div>
   </div>
